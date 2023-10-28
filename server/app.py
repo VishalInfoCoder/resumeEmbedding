3bd2ce5e6c8a8d8b55347f4c2009851f16995539
raja <rajapandik06@gmail.com>
2023-10-28 09:02:38 +0530

    error decoded

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore")
 persist_directory='database'
 app = Flask(__name__)
 from openAi_config import main
-embedding_function = main()
+embeddingFunction = main()
 # print(embeddingFunction)
 @app.route('/api/embeddings/CreateEmbeddings', methods=['POST'])
 def hello_world():
@@ -34,9 +34,12 @@ def hello_world():
 def get_data():
      data = request.get_json()
      vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddingFunction) 
-     search_query="java and springboot developer with 2+ years of experience"
-     results = vectordb.similarity_search_with_score(search_query,k=3)
-     return make_response({'data':results,"status":True}) 
+    #  search_query="java and springboot developer with 2+ years of experience"
+     results = vectordb.similarity_search_with_score(data["search_query"],k=3)
+     print(results[0][0].metadata["id"])
+     myResult=[result[0].metadata["id"] for result in results]
+     print(myResult)
+     return make_response({'data':myResult,"status":True}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
